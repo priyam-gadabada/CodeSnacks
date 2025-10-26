3fc21d7909fc6637645a79c4dbd05e6e11c0af29
Priyam Gadabada <priyamgadabada@gmail.com>
2025-10-26 23:16:06 +0530

    Add a Simple Calculator to CodeSnacks
    This is a Simple Calculator made with Python.

---
+++ Simple Calculator/SimpleCalc.py
@@ -0,0 +1,14 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Enter operation (+, -, *, /): ")
+
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+elif op == "/":
+    print(a / b)
+else:
+    print("Invalid operation!")
