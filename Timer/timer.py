37fddc268fb336f8c2def3f7ba8fd9eeb371bbfd
Priyam Gadabada <priyamgadabada@gmail.com>
2025-11-01 23:17:28 +0530

    Implement countdown timer in timer.py

---
+++ Timer/timer.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = f'{mins:02d}:{secs:02d}'
+        print(timer, end='\r')
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+    print('\a')
+
+time_in_seconds = int(input("Enter the time in seconds: "))
+countdown_timer(time_in_seconds)
